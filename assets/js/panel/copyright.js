550b95fd2a02500c004c3c04fcd55030d73015e1
DelWare <86409467+kinseyy@users.noreply.github.com>
2022-04-14 12:03:42 -0700

    Add files via upload

---
+++ assets/js/panel/copyright.js
@@ -0,0 +1,9 @@
+/**
+ * @file copyright.js
+ * @author ENDER#4335
+ * @license GPL-3.0
+ */
+
+$('.000').replaceWith('Copyright © 2021');
+$('.001').replaceWith('ENDER#4335');
+$('.002').replaceWith('All rights reserved.');
